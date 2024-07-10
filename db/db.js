37c4b5b4954c9ac6c501899bfeabbe0b3d277aaa
Pankaj2k01 <yadavpankaj092785@gmail.com>
2024-07-10 13:42:14 +0530

    login

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongo_uri = process.env.MONGO_URI;
-
+require('dotenv').config()
+const mongo_uri = process.env.MONGO_URI 
 mongoose
     .connect('mongo_uri')
     .then(() => console.log("Connected to database"))
