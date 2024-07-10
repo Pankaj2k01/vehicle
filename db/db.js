29464f275e2a573e104772c6cac19a441420f2b2
Pankaj2k01 <yadavpankaj092785@gmail.com>
2024-07-10 15:09:40 +0530

    login

--- db/db.js
+++ db/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const mongo_uri = process.env.MONGO_URI;
 
 mongoose
-    .connect('mongodb+srv://yadavpankaj092785:O5CKKPf56NJaGDyi@cluster0.dp7un9n.mongodb.net/authtest')
+    .connect(mongo_uri)
     .then(() => console.log("Connected to database"))
     .catch(() => console.log("Error connecting to database"));
 
@@ -9,7 +11,7 @@ const authSchema = new mongoose.Schema({
 
     name: {
         type: String,
-        requied: true
+        required: true
     },
     email: {
         type: String,
@@ -18,10 +20,10 @@ const authSchema = new mongoose.Schema({
     },
     pass: {
         type: String,
-        requied: true
+        required: true
     }
 });
 
-const auth = mongoose.model('auth', authSchema, 'creds');
+const creds = mongoose.model('creds', authSchema, );
 
-module.exports = auth;
+module.exports = creds;
